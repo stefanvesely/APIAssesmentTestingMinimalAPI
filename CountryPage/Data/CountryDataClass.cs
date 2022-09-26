a776763e308145ff762700e11fa584c1f22ab3f2
stefanvesely <baws.steff@gmail.com>
2022-09-26 22:11:37 +0200

    260922

---
+++ CountryPage/Data/CountryDataClass.cs
@@ -0,0 +1,27 @@
+﻿using CountryPage.Models;
+
+namespace CountryPage.Data
+{
+    public class CountryDataClass : ICountryDataClass
+    {
+        public Task<List<CountryModel>> GetAllCountries()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<CountryModel>> GetCountriesWithCurrancyCode(string CurrancyCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<CountryModel>> GetCountriesWithCurrancyName(string CurrancyCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CountryModel> GetCountryAlpha(string Alpha, int AlphaCode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CountryPage/Data/ICountryDataClass.cs
@@ -0,0 +1,14 @@
+﻿using CountryPage.Models;
+using Microsoft.AspNetCore.Routing.Constraints;
+
+namespace CountryPage.Data
+{
+    public interface ICountryDataClass 
+    {
+        public Task<List<CountryModel>> GetAllCountries();
+        public Task<CountryModel> GetCountryAlpha(string Alpha, int AlphaCode);
+        public Task<List<CountryModel>> GetCountriesWithCurrancyCode(string CurrancyCode);
+        public Task<List<CountryModel>> GetCountriesWithCurrancyName(string CurrancyCode);
+
+    }
+}
